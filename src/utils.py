fbe42452d2a7ca50007de14cf00843493c51439c
Kyoichi Iwasaki <kyiwasak@microsoft.com>
2023-08-30 13:41:16 +0000

    Updated utils.py

--- src/utils.py
+++ src/utils.py
@@ -4,12 +4,12 @@ import time
 import requests
 from flickrapi import FlickrAPI
 import azure.ai.vision as sdk
-
 import numpy as np
+import pandas as pd
 
 class AzureImageRetrieval():
     def __init__(self,
-                 config_file: str = 'config.yml',) -> None:
+                 config_file: str) -> None:
         ## Prepare config file
         self.config_file = config_file
         self.load_config()
@@ -17,15 +17,24 @@ class AzureImageRetrieval():
         self.API_KEY = self.config['flickr']['API_KEY']
         self.API_SECRET = self.config['flickr']['API_SECRET']
         self.NUMBER_OF_IMAGES = self.config['flickr']['NUMBER_OF_IMAGES']
+        self.NUMBER_PROCESS_IMAGES = self.config['flickr']['NUMBER_PROCESS_IMAGES']
         ## Configuaration in Azure
         self.CV_ENDPOINT = self.config['Azure']['ENDPOINT']
         self.CV_KEY = self.config['Azure']['KEY']
         self.endpoint = self.CV_ENDPOINT + '/computervision/retrieval:vectorizeImage?api-version=2023-02-01-preview&modelVersion=latest'
-        self.header = {
+        self.headers = {
             "Content-Type": "application/octet-stream",  # binary data in sending API
             "Ocp-Apim-Subscription-Key": self.CV_KEY
         }
-        self.vectors = []
+        self.vectors = dict()
+        ## initialization of image SDK
+        self.analysis_options = sdk.ImageAnalysisOptions()
+        self.service_options = sdk.VisionServiceOptions(self.CV_ENDPOINT,
+                                                        self.CV_KEY)
+
+        ## VectorDB
+        self.df = None
+        self.vectorDB = self.config['app']['vectorDB']
 
     def load_config(self):
         '''
@@ -88,6 +97,37 @@ class AzureImageRetrieval():
             print(e)
             raise
 
+    def getImageProperties(self,
+                   image:str) -> str:
+        try:
+            self.analysis_options.features = (
+                sdk.ImageAnalysisFeature.CROP_SUGGESTIONS |
+                sdk.ImageAnalysisFeature.CAPTION |
+                sdk.ImageAnalysisFeature.DENSE_CAPTIONS |
+                sdk.ImageAnalysisFeature.OBJECTS |
+                sdk.ImageAnalysisFeature.PEOPLE |
+                sdk.ImageAnalysisFeature.TEXT |
+                sdk.ImageAnalysisFeature.TAGS
+            )
+            self.analysis_options.language = "en"
+            self.analysis_options.gender_neutral_caption = True
+            vision_source = sdk.VisionSource(filename=image)
+            image_analyzer = sdk.ImageAnalyzer(self.service_options
+                                               , vision_source
+                                               , self.analysis_options)
+            ## Analyze image
+            return image_analyzer.analyze()
+        except Exception as e:
+            print(e)
+            raise
+
+    def convertDataFrame(self):
+        try:
+            self.df = pd.DataFrame(self.vectors).T
+        except Exception as e:
+            print(e)
+            raise
+
     def getVectorFromImages(self,
                             folder: str):
         '''
@@ -96,11 +136,46 @@ class AzureImageRetrieval():
         '''
         try:
             ## Get image file name
-            images = [image for image in os.listdir() if image.endswith('.jpg')]
+            images = [image for image in os.listdir(folder) if image.endswith('.jpg')]
+            print(images)
             ## Analyze each image with API
-            for image in images:
-                vector = self.getVector(image)
-                self.vectors.append(vector)
+            for image in images[:self.NUMBER_PROCESS_IMAGES]:
+                ## Set image path
+                image_path = os.path.join(folder, image)
+                ## Get vector for image
+                vector = self.getVector(image=image_path)
+                print(vector)
+                ## Analyze with Azure Vision API
+                analyzed_result = self.getImageProperties(image=image_path)
+                try:
+                    caption_content = analyzed_result.caption.content
+                except:
+                    caption_content = None
+
+                ## Store the results
+                self.vectors[image] = {}
+                self.vectors[image]['vector'] = vector
+#                self.vectors[image]['properties'] = analyzed_result
+                self.vectors[image]['caption'] = caption_content
+                time.sleep(1.5)
+            ## Convert to DataFrame
+            self.convertDataFrame()
         except Exception as e:
             print(e)
             raise
+
+    def storeDataFrame(self):
+        try:
+            self.df.to_pickle(self.vectorDB)
+        except Exception as e:
+            print(e)
+            raise
+
+    def loadDataFrame(self):
+        try:
+            self.df = pd.read_pickle(self.vectorDB)
+        except Exception as e:
+            print(e)
+            raise
+
+
