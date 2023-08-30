ef657208fbd0c434e5712e1488b90def4839ef68
Kyoichi Iwasaki <kyiwasak@microsoft.com>
2023-08-30 10:07:31 +0000

    Added vector search feature in utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,11 @@
-import os
+import os, sys
 import yaml
 import time
 import requests
 from flickrapi import FlickrAPI
 import azure.ai.vision as sdk
+
+import numpy as np
 
 class AzureImageRetrieval():
     def __init__(self,
@@ -16,9 +18,14 @@ class AzureImageRetrieval():
         self.API_SECRET = self.config['flickr']['API_SECRET']
         self.NUMBER_OF_IMAGES = self.config['flickr']['NUMBER_OF_IMAGES']
         ## Configuaration in Azure
-        self.CV_ENDPOINT = self.config['azure']['ENDPOINT']
-        self.CV_KEY = self.config['azure']['KEY']
-
+        self.CV_ENDPOINT = self.config['Azure']['ENDPOINT']
+        self.CV_KEY = self.config['Azure']['KEY']
+        self.endpoint = self.CV_ENDPOINT + '/computervision/retrieval:vectorizeImage?api-version=2023-02-01-preview&modelVersion=latest'
+        self.header = {
+            "Content-Type": "application/octet-stream",  # binary data in sending API
+            "Ocp-Apim-Subscription-Key": self.CV_KEY
+        }
+        self.vectors = []
 
     def load_config(self):
         '''
@@ -67,4 +74,33 @@ class AzureImageRetrieval():
             print(e)
             raise
 
+    def getVector(self, 
+                  image: str) -> np.array:
+        '''
+        Get vector with API for one image
+        '''
+        try:
+            with open(image, mode="rb") as f:
+                image_bin = f.read()
+            response = requests.post(self.endpoint, headers=self.headers, data=image_bin)
+            return np.array(response.json()['vector'], dtype='float32')
+        except Exception as e:
+            print(e)
+            raise
 
+    def getVectorFromImages(self,
+                            folder: str):
+        '''
+        - input:
+            folder: Local folder name including images
+        '''
+        try:
+            ## Get image file name
+            images = [image for image in os.listdir() if image.endswith('.jpg')]
+            ## Analyze each image with API
+            for image in images:
+                vector = self.getVector(image)
+                self.vectors.append(vector)
+        except Exception as e:
+            print(e)
+            raise
